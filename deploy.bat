b34ef17d0c825c556eb3e32b319f7eae44dcd6aa
tuanphamuac <87295433+tuanphamuac@users.noreply.github.com>
2022-06-20 17:50:37 +0700

    update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,15 @@
 echo "Building with yarn"
-@REM rm -rf build/
-@REM yarn build:qa
 
 set AWS_PROFILE=%1
 set S3_DIR="s3://sound-rig-web"
-CLOUDFRONT_DIST_ID="EW6DUEBWUWNY2"
+set CLOUDFRONT_DIST_ID="EW6DUEBWUWNY2"
+
+if [%2]==[no-build] (GOTO:no-build)
+if [%2]==[clear-cache] (GOTO:clear-cache)
+
+rm -rf build/
+yarn build:qa
+:no-build
 
 echo "Deploying to %S3_DIR%"
 
@@ -14,6 +19,7 @@ aws s3 --profile=%AWS_PROFILE% rm %S3_DIR% --recursive
 echo "Deploying build/ to $S3_DIR"
 aws s3 --profile=%AWS_PROFILE% cp build/ %S3_DIR% --recursive --exclude "*.map"
 
-@REM echo "Invalidate CDN"
-@REM The charge to submit an invalidation path is the same regardless of the number of files, so it's better to invalidate everything for the peace of mind
-@REM aws cloudfront --profile=$AWS_PROFILE create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"
+:clear-cache
+echo "Invalidate CDN"
+The charge to submit an invalidation path is the same regardless of the number of files, so it's better to invalidate everything for the peace of mind
+aws cloudfront --profile=%AWS_PROFILE% create-invalidation --distribution-id=%CLOUDFRONT_DIST_ID% --paths "/*"
